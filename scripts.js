92d20f84eaa289e569d961d635c732c85972b7da
Moaz Ahmed <moaz.ahmed132@gmail.com>
2020-03-23 23:44:23 +0200

    23/3/2020

--- scripts.js
+++ scripts.js
@@ -1,52 +1,71 @@
 let hasFlipped = false;
-let firsFlip , secondFlip ;
+let firsFlip, secondFlip;
 let lockBoard = true;
 const cards = document.querySelectorAll('.memory-card');
-function flipCard(){
-    if(!lockBoard){return;}
-        console.log("lockBoard dont work");
+
+cards.forEach(card => {
+    card.classList.add('flip');
+    setTimeout(function () {
+        card.classList.remove('flip');
+
+    }, 1500);
+
+
+
+});
+
+function flipCard() {
+    if (!lockBoard) {
+        return;
+    }
     this.classList.toggle('flip');
-    if(!hasFlipped){
-        hasFlipped = true ; 
+    if (!hasFlipped) {
+        hasFlipped = true;
         firsFlip = this;
-        firsFlip.removeEventListener('click',flipCard);
-    
-    }else{
-        
-         secondFlip = this ; 
-         hasFlipped = false;
-         secondFlip.removeEventListener('click',flipCard);
-         lockBoard = false;
+        firsFlip.removeEventListener('click', flipCard);
+
+
+    } else {
+
+        secondFlip = this;
+        hasFlipped = false;
+        secondFlip.removeEventListener('click', flipCard);
+        lockBoard = false;
 
 
 
-         if(firsFlip.dataset.cartype === secondFlip.dataset.cartype){
-             console.log("matchinng");
-             firsFlip.removeEventListener('click',flipCard);
-             secondFlip.removeEventListener('click',flipCard);
-             lockBoard = true;
+        if (firsFlip.dataset.cartype === secondFlip.dataset.cartype) {
+            cardsMatched();
 
-
-         }else{
-             setTimeout (function(){
-                firsFlip.classList.remove('flip');
-                secondFlip.classList.remove('flip');
-                firsFlip.addEventListener('click',flipCard);
-                secondFlip.addEventListener('click',flipCard);
-                lockBoard = true;
-
-            },1000);
-             }
-            
-         }
-
-    
+        } else
+            cardsNotMatching();
+    }
 }
-(function shuffle(){
+(function shuffle() {
     cards.forEach(card => {
         let randCard = Math.floor(Math.random() * 12);
         card.style.order = randCard;
     });
 })();
-cards.forEach(card => card.addEventListener('click',flipCard));
+cards.forEach(card => card.addEventListener('click', flipCard));
 
+function cardsMatched() {
+    console.log("matchinng");
+    firsFlip.removeEventListener('click', flipCard);
+    secondFlip.removeEventListener('click', flipCard);
+    lockBoard = true;
+
+}
+
+function cardsNotMatching() {
+    {
+        setTimeout(function () {
+            firsFlip.classList.remove('flip');
+            secondFlip.classList.remove('flip');
+            firsFlip.addEventListener('click', flipCard);
+            secondFlip.addEventListener('click', flipCard);
+            lockBoard = true;
+
+        }, 1000);
+    }
+}
